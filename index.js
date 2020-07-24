b548cf7f7e205270137b8d62c80655ba81354f91
Arnav Kolhe <arnavkolhe@gmail.com>
2020-07-24 09:33:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client } = require("discord.js");
+const {prefix,token} = require('./config.json');
+const bot = new Client();
+var logoutOfDiscord = false;
+
+
+//bot.destroy(token);
+
+
+bot.on('message',(message) => {
+    if(message.content.startsWith(`${prefix}`)){
+     if (message.content == (`${prefix}quit`)){
+         message.channel.send("Goodbye");
+         logoutOfDiscord= true;         
+     } 
+    }
+ });
+
+if (logoutOfDiscord == true){
+    bot.destroy();
+}
+
+bot.login(token);
