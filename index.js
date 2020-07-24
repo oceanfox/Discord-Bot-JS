233f00d9d2d51ddd1a4cbf747bfaa62d6c4a34d2
oceanfox <arnavkolhe@gmail.com>
2020-07-24 11:43:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client } = require("discord.js");
-const {prefix,token} = require('./config.json');
+//const {prefix,token} = require('./config.json');
 const bot = new Client();
 var logoutOfDiscord = false;
 
@@ -8,8 +8,8 @@ var token = process.env.DISCORD_TOKEN;
 
 
 bot.on('message',(message) => {
-    if(message.content.startsWith(`${prefix}`)){
-     if (message.content == (`${prefix}quit`)){
+    if(message.content.startsWith('...')){
+     if (message.content == ('...quit')){
          message.channel.send("Goodbye");
          logoutOfDiscord= true;         
      } 
